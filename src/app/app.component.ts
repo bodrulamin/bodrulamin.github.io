d83526babeb8f3984b6634c8a73f1b725124264d
bodrulamin <bodrulaminiu@gmail.com>
2024-11-22 14:40:41 +0600

    clean up date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,13 @@ import {PanelModule} from "primeng/panel";
 import {DataService} from "./data/data.service";
 import {CardModule} from "primeng/card";
 import {ChipModule} from "primeng/chip";
-import {jsonData} from "./data/dummy.data";
 import {TagModule} from "primeng/tag";
 import {InputSwitchModule} from "primeng/inputswitch";
 import {FormsModule} from "@angular/forms";
 import {TimelineModule} from "primeng/timeline";
 import {CarouselModule} from "primeng/carousel";
 import {MenuModule} from "primeng/menu";
+import {jsonData} from "./data/dummy.data";
 
 
 @Component({
@@ -30,106 +30,14 @@ import {MenuModule} from "primeng/menu";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'bodrulamin';
   isDarkMode = true;
-
-  social = {
-    title: '',
-    value: '', canCopy: true,
-
-    onClick: () => {
-    }
-  };
   severities = ["success", "secondary", "info", "warning", "danger", "contrast"];
-  events = [
-    {
-      designation: 'Software Engineer',
-      company: 'Millennium Information Solution Ltd.',
-      date: 'Sep 2024 – Present',
-      description: 'Working as a developer in an agile team, contributing to Ababil-NG, one of the top 3 Islamic Core Banking Solutions in Bangladesh.\n' +
-        'Responsibilities include designing, coding, and delivering new modules, adding new features, optimizing performance, and fixing bugs.\n' +
-        'Managing the Swift-Middleware project.\n' +
-        'Reviewing code from junior developers and mentoring them on best practices.',
-      image: null,
-      color: '#007BFF',
-      icon: 'pi pi-briefcase'
-    },
-    {
-      designation: 'Assistant Software Engineer',
-    company: 'Millennium Information Solution Ltd.',
-      date: 'May 2022 – Aug 2024',
-      description: 'Contributed to multiple microservice projects using Java Spring Boot, adding new features and improving performance.\n' +
-        'Developed 4 new libraries for Ababil-NG using Angular, JavaScript, and Typescript while optimizing existing libraries.',
-      image: null,
-      color: '#28A745',
-      icon: 'pi pi-code'
-    },
-    {
-      designation: 'Junior Software Engineer',
-      company: 'Al-Khwarizmi Software Lab',
-      date: 'Dec 2020 – Apr 2022',
-      description: 'Developed REST API-based web applications using Spring Boot.\n' +
-        'Built the Shikkhon Android application, a mobile library app. 20k downloads in playstore',
-      image: null,
-      color: '#17A2B8',
-      icon: 'pi pi-mobile'
-    },
-  ];
-  menuItems = [
-    { label: 'About', command: () => this.scrollToSection('aboutMe') },
-    { label: 'Skills', command: () => this.scrollToSection('skill') },
-    { label: 'Experience', command: () => this.scrollToSection('experience') },
-    { label: 'Portfolio', command: () => this.scrollToSection('portfolio') },
-    { label: 'Contact Me', command: () => this.scrollToSection('contactMe') },
-    { label: this.isDarkMode ? 'Light Mode' : 'Dark Mode', icon: 'pi pi-moon', command: () => this.toggleDarkMode() },
-  ];
-
-  scrollToSection(sectionId: string): void {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
+  menuItems: any[] = [];
   severityMap = new Map();
-  private socials: any;
-  jsonData = jsonData;
-  icons= new Map([['Java','fa-brands fa-java']]);
+  jsonData= jsonData;
+  private severityIndex: number = Math.floor(Math.random() * this.severities.length);
 
   constructor(private dataService: DataService) {
-    this.socials = {
-      'linkedin': {
-        title: 'LinkedIn',
-        value: 'https://linkedin.com/in/bodrulamin', canCopy: true,
-        onClick: () => {
-          window.open(this.social.value);
-        }
-      },
-      'github': {
-        title: 'Github',
-        value: 'https://github.com/bodrulamin', canCopy: true,
-        onClick: () => {
-          window.open(this.social.value);
-        }
-      },
-      'email': {
-        title: 'Email',
-        value: 'bodrulaminiu@gmail.com', canCopy: true,
-        onClick: () => {
-          window.open('mailto:' + this.social.value);
-        }
-      },
-      'phone': {
-        title: 'Phone Number',
-        value: '+8801725717136', canCopy: true,
-        onClick: () => {
-          window.open('tel:' + this.social.value);
-        }
-      },
-
-
-    }
-
-
     this.dataService.getJsonData().subscribe({
         next: data => {
           this.jsonData = data;
@@ -140,67 +48,39 @@ export class AppComponent {
         }
       }
     );
-
+    this.updateMenu();
   }
 
-  onSocialClicked(selectedSocial: string) {
-    this.social = this.socials[selectedSocial];
-    this.social.onClick();
+  onSocialClicked(selectedSocial: any) {
+    window.open(selectedSocial.prefix + selectedSocial.value);
   }
 
-  copyValue() {
 
-  }
-
-  // Function to get a random severity
-  private severityIndex: number = Math.floor(Math.random() * this.severities.length);
-  getRandomSeverity(s:any): any {
-    this.severityIndex = this.severities.length > this.severityIndex ? ++this.severityIndex: this.severityIndex = 0 ;
-    if(this.severityMap.get(s)){
+  getRandomSeverity(s: any): any {
+    this.severityIndex = this.severities.length > this.severityIndex ? ++this.severityIndex : this.severityIndex = 0;
+    if (this.severityMap.get(s)) {
       this.severityMap.get(s)
     } else {
-      this.severityMap.set(s,this.severities[this.severityIndex]) ;
-          }
+      this.severityMap.set(s, this.severities[this.severityIndex]);
+    }
     return this.severityMap.get(s)
   }
 
-
-  #document = inject(DOCUMENT);
-  portfolioProjects = [
-    {
-      title: 'Project 1',
-      subtitle: 'Web Application',
-      description: 'A web application built using Angular and Spring Boot.',
-      image: 'https://via.placeholder.com/400x200',
-      link: 'https://github.com/your-project-link'
-    },
-    {
-      title: 'Project 2',
-      subtitle: 'Mobile App',
-      description: 'A mobile app developed with Flutter for seamless user experience.',
-      image: 'https://via.placeholder.com/400x200',
-      link: 'https://play.google.com/store/apps/details?id=your-app-link'
-    },
-    {
-      title: 'Project 3',
-      subtitle: 'E-commerce Platform',
-      description: 'An e-commerce platform for managing online stores.',
-      image: 'https://via.placeholder.com/400x200',
-      link: 'https://your-ecommerce-project-link.com'
-    }
-  ];
   toggleDarkMode() {
-    const linkElement = this.#document.getElementById(
+    const linkElement = document.getElementById(
       'app-theme',
     ) as HTMLLinkElement;
     if (linkElement.href.includes('light')) {
       linkElement.href = 'theme-dark.css';
       this.isDarkMode = true;
+      this.updateMenu();
     } else {
       linkElement.href = 'theme-light.css';
       this.isDarkMode = false;
+      this.updateMenu();
     }
   }
+
   contactData = {
     name: '',
     email: '',
@@ -231,6 +111,29 @@ export class AppComponent {
       // You can integrate this data with a backend API to handle submissions
       alert('Message sent successfully!');
       contactForm.reset();
+    }
+  }
+
+  private updateMenu() {
+    this.menuItems = [
+      {label: 'About', command: () => this.scrollToSection('aboutMe')},
+      {label: 'Skills', command: () => this.scrollToSection('skill')},
+      {label: 'Experience', command: () => this.scrollToSection('experience')},
+      {label: 'Portfolio', command: () => this.scrollToSection('portfolio')},
+      {label: 'Contact Me', command: () => this.scrollToSection('contactMe')},
+      {
+        label: this.isDarkMode ? 'Light Mode' : 'Dark Mode',
+        id: 'theme',
+        icon: this.isDarkMode ? 'pi pi-sun' : 'pi pi-moon',
+        command: () => this.toggleDarkMode()
+      },
+    ];
+  }
+
+  scrollToSection(sectionId: string): void {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({behavior: 'smooth'});
     }
   }
 
--- src/app/data/dummy.data.ts
+++ src/app/data/dummy.data.ts
@@ -2,26 +2,92 @@ export const jsonData = {
   aboutMe: "Software engineer with 4 years of experience in Java, Spring Boot, and Angular. Skilled in optimizing\n" +
     "high-load systems, AWS cloud, and resolving customer issues. Experienced in on-call support and mentoring junior developers.\n" +
     "Focused on building scalable applications and improving system performance.",
-  contact: [
-    "Phone = +8801725717136",
-    "LinkedIn = https://linkedin/in/bodrulamin",
-    "Github = https://github.com/bodrulamin",
-    "Website = https://bodrulamin.github.io",
-  ],
+  socials: {
+    'linkedin': {
+      title: 'LinkedIn',
+      value: 'https://linkedin.com/in/bodrulamin',
+      prefix: ''
+    },
+    'github': {
+      title: 'Github',
+      value: 'https://github.com/bodrulamin',
+      prefix: ''
+    },
+    'email': {
+      title: 'Email',
+      value: 'bodrulaminiu@gmail.com',
+      prefix: ('mailto:')
+
+    },
+    'phone': {
+      title: 'Phone Number',
+      value: '+8801725717136',
+      prefix: 'tel:'
+    },
+
+  },
   skill: [
-   "Java, Javascript, Typescript, Spring Boot, Hibernate, Angular, AngularJS, PostgreSQL, Oracle, Firestore"
+    "Java, Javascript, Typescript, Spring Boot, Hibernate, Angular, AngularJS, PostgreSQL, Oracle, Firestore"
   ],
   experience: [
     {
-      "title": "Software Engineer",
-      "timeline": "Aug 2002 - present",
-      "institutionName": "Millennium Information Solution Ltd.",
-      "keyPoints": [
-        "lorem ipsum",
-        "lorem ipsum dolor",
-      ]
+      designation: 'Software Engineer',
+      company: 'Millennium Information Solution Ltd.',
+      date: 'Sep 2024 – Present',
+      description: 'Working as a developer in an agile team, contributing to Ababil-NG, one of the top 3 Islamic Core Banking Solutions in Bangladesh.\n' +
+        'Responsibilities include designing, coding, and delivering new modules, adding new features, optimizing performance, and fixing bugs.\n' +
+        'Managing the Swift-Middleware project.\n' +
+        'Reviewing code from junior developers and mentoring them on best practices.',
+      image: null,
+      color: '#007BFF',
+      icon: 'pi pi-briefcase'
+    },
+    {
+      designation: 'Assistant Software Engineer',
+      company: 'Millennium Information Solution Ltd.',
+      date: 'May 2022 – Aug 2024',
+      description: 'Contributed to multiple microservice projects using Java Spring Boot, adding new features and improving performance.\n' +
+        'Developed 4 new libraries for Ababil-NG using Angular, JavaScript, and Typescript while optimizing existing libraries.',
+      image: null,
+      color: '#28A745',
+      icon: 'pi pi-code'
+    },
+    {
+      designation: 'Junior Software Engineer',
+      company: 'Al-Khwarizmi Software Lab',
+      date: 'Dec 2020 – Apr 2022',
+      description: 'Developed REST API-based web applications using Spring Boot.\n' +
+        'Built the Shikkhon Android application, a mobile library app. 20k downloads in playstore',
+      image: null,
+      color: '#17A2B8',
+      icon: 'pi pi-mobile'
+    },
+  ],
+
+  portfolioProjects: [
+    {
+      title: 'Project 1',
+      subtitle: 'Web Application',
+      description: 'A web application built using Angular and Spring Boot.',
+      image: 'https://via.placeholder.com/400x200',
+      link: 'https://github.com/your-project-link'
+    },
+    {
+      title: 'Project 2',
+      subtitle: 'Mobile App',
+      description: 'A mobile app developed with Flutter for seamless user experience.',
+      image: 'https://via.placeholder.com/400x200',
+      link: 'https://play.google.com/store/apps/details?id=your-app-link'
+    },
+    {
+      title: 'Project 3',
+      subtitle: 'E-commerce Platform',
+      description: 'An e-commerce platform for managing online stores.',
+      image: 'https://via.placeholder.com/400x200',
+      link: 'https://your-ecommerce-project-link.com'
     }
   ],
+
   cirtification: [
     {
       "title": "Java",
